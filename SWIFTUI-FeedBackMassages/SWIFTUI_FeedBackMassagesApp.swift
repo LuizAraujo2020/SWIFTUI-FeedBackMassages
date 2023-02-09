1eaa707d17b188ec8c3a79ceafdf6d5b4802a574
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-02-09 09:22:55 -0300

    Initial Commit

---
+++ SWIFTUI-FeedBackMassages/SWIFTUI_FeedBackMassagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTUI_FeedBackMassagesApp.swift
+//  SWIFTUI-FeedBackMassages
+//
+//  Created by Luiz Araujo on 09/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUI_FeedBackMassagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
